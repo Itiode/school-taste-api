72d0a2ddc0c785c4479091c70694de05a7175504
Itiode <itiodecompany@gmail.com>
2021-12-30 03:27:29 +0100

    Update

--- dist/models/schemas/user-image.js
+++ dist/models/schemas/user-image.js
@@ -3,11 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
 exports.default = new mongoose_1.Schema({
     original: {
-        url: { type: String, trim: true, required: true },
-        dUrl: { type: String, trim: true, required: true },
+        url: { type: String, trim: true },
+        dUrl: { type: String, trim: true },
     },
     thumbnail: {
-        url: { type: String, trim: true, required: true },
-        dUrl: { type: String, trim: true, required: true },
+        url: { type: String, trim: true },
+        dUrl: { type: String, trim: true },
     },
 }, { _id: false });
--- src/models/schemas/user-image.ts
+++ src/models/schemas/user-image.ts
@@ -5,12 +5,12 @@ import { UserImage } from "../../types/user";
 export default new Schema<UserImage>(
   {
     original: {
-      url: { type: String, trim: true, required: true },
-      dUrl: { type: String, trim: true, required: true },
+      url: { type: String, trim: true },
+      dUrl: { type: String, trim: true },
     },
     thumbnail: {
-      url: { type: String, trim: true, required: true },
-      dUrl: { type: String, trim: true, required: true },
+      url: { type: String, trim: true },
+      dUrl: { type: String, trim: true },
     },
   },
   { _id: false }
